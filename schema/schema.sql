ad5873ef56b01d0e460155bfc10dbab8c27d6adf
Ratikanat Pradhan <ratikantapradhan534@gmail.comgit config --global user.email ratikantapradhan534@gmail.com>
2025-07-10 12:47:55 +0530

    Add:templete schema for database

---
+++ schema/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS react_auth;
+USE user_registration;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
